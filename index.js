340c68820a523dae327cebb04fc85ea6502840db
Nando <hola@holasocnando.com>
2023-01-07 00:11:51 +0100

    created User controller

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import register from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +34,9 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose.set('strictQuery', false);
@@ -42,3 +46,4 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 }).catch((error) => console.log(`${error} did not connect.`));
+
