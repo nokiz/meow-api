8f227bedacd102bdaba8c246dc0c06ab618f6914
Nando <hola@holasocnando.com>
2023-01-08 22:42:29 +0100

    Fixed typo in routes

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-app.user("/posts", postRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
